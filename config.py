23770262f9b791f1da3196c4f8748d64309e60da
chillancezen <chillancezen@gmail.com>
2015-08-26 22:46:27 -0400

    update

--- config.py
+++ config.py
@@ -1,1 +1,35 @@
 #! /usr/bin/python
+
+import sys
+class config():
+	
+	def __init__(self,file):
+		self.conf=self.resolve_config(file)
+			
+	def resolve_config(self,file):
+		ret=dict()
+		with open(file) as f:
+			for line in f:
+				line=line.strip(" ")
+				line=line.strip("\n")
+				line=line.strip("\r")
+				if line=="" or line[0]=='#':
+					continue
+				idx=line.find("=")
+				if idx == -1:
+					continue
+				key=line[:idx].strip()
+				val=line[idx+1:].strip()
+				ret[key]=val
+		return ret
+	def key_exist(self,key):
+		return key in self.conf
+	def get_value(self,key):
+		try:
+			return self.conf[key]
+		except:
+			return None
+
+if __name__ =="__main__":
+	conf=config(sys.argv[1])
+	print conf.get_value("denable_ipset")
