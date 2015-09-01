37a8df47c923d8223ccf15e834c6d31865ab6f3d
chillancezen <chillancezen@gmail.com>
2015-09-01 06:11:28 -0400

    node management updated

--- util/node_mng.py
+++ util/node_mng.py
@@ -1,34 +1,47 @@
 #! /usr/bin/python
 import random
+import uuid
+import threading
+
 class node_info:
+
 	"""
 	node information which identifies the unique underlayer node
 	"""
 	def __init__(self,id,uuid=None,addr=None):
 		self.node_id=id
-		self.uuid=uuid
-		self.addr=addr
+		self.uuid=uuid #identifier used by super node
+		self.addr=addr# identifier used by index node
 		self.timer_cnt=0
 		
 	def node_init_callback(self):
 		
 		pass
 	def node_delete_callback(self):
-	
+
 		pass
 	def node_expiry_callback(self):
-
+		print "expiry"
 		pass
 class node_mng_entity:
 	"""
 	node management entity,there should be only one 
 	instance on index node or super node
 	"""
+	age_exit_flag=False
+	nme_lock=threading.Lock()
+	node_tbl=dict()
+	node_expiry_time=20 #seconds age timers
+	node_id_pool=set()
+	node_uuid_pool=set()
 	def __init__(self):
-		self.node_tbl=dict()
-		self.node_expiry_time=300 #seconds age timers
-		self.node_id_pool=set()
-		self.node_uuid_pool=set()
+
+		#self.lock=threading.Lock()
+		#spawn the age timer threading
+		self.age_pid=threading.Thread(target=age_timer,kwargs={"nme":self})
+		self.age_pid.setDaemon(True)
+		self.age_pid.start()
+
 
 	def random_string(self,len):#generate random string
 		str=""
@@ -43,6 +56,14 @@ class node_mng_entity:
 				break
 		self.node_id_pool.add(str)
 		return str
+	def node_uuid_allocate(self):
+		uid=""
+		while True:
+			uid=uuid.uuid4()
+			if uid  not in self.node_uuid_pool:
+				break
+		self.node_uuid_pool.add(uid)
+		return uid
 
 	def node_alloc(self,node_type,**arg):
 		return node_type(**arg)	
@@ -63,21 +84,50 @@ class node_mng_entity:
 
 	def node_delete(self,key_id):
 		if self.node_find(key_id) is not None:
+			self.node_tbl[key_id].node_delete_callback()#call delete routine
 			del self.node_tbl[key_id]
-	def node_update(self):
+	def node_update(self,key_id):
+		self.nme_lock.acquire()
+		if key_id in self.node_tbl:
+			self.node_tbl[key_id].timer_cnt=0
+		self.nme_lock.release()
+		pass
 
-		pass
-	def age_timer(self):
-		"""
-		age timer thread 
-		"""
-		pass
+	def stop_age_timer(self):
+		self.age_exit_flag=True
+		self.age_pid.join()#wait for thread finishing
+
+def age_timer(nme):
+	"""
+		age timer thread
+	"""
+	interval_time=2
+
+	while nme.age_exit_flag is False:#thread not safe,but it does no matter
+		threading._sleep(interval_time)
+		nme.nme_lock.acquire()
+		del_lst=set()
+		#traverse the node table,fidn these expired node,and recollect them
+		for nod in nme.node_tbl:
+			if nme.node_tbl[nod].timer_cnt > nme.node_expiry_time:
+				nme.node_tbl[nod].node_expiry_callback()
+				del_lst.add(nod)
+			else :
+				nme.node_tbl[nod].timer_cnt+=interval_time
+		for ele in del_lst:
+			if ele in nme.node_tbl:
+				del nme.node_tbl[ele]
+		nme.nme_lock.release()
 
 if __name__ == "__main__":
 	nme=node_mng_entity()
 	node=nme.node_alloc(node_info,**{"id":nme.node_id_allocate(),"addr":"192.168.6.1"})
 	nme.node_register(node.node_id,node)
-	nme.node_delete(node.node_id)
-
+	#nme.node_delete(node.node_id)
+	threading._sleep(500)
+	nme.stop_age_timer()
 	node_bak=nme.node_find(node.node_id)
 	print node_bak.node_id
+	print nme.node_uuid_allocate()
+	print nme.node_uuid_allocate()
+	print nme.node_uuid_allocate()
